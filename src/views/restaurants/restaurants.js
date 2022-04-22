54e2a0eabc2f2b65a11c82471c50df772172b603
kisielewskik <k.kisielewski.k@gmail.com>
2022-04-22 11:36:34 +0200

    feat: add missing files after repo transfer

--- src/views/restaurants/restaurants.js
+++ src/views/restaurants/restaurants.js
@@ -8,30 +8,34 @@ const Restaurants = () => {
   const location = useLocation();
 
   const getRestaurants = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/restaurant');
-  return data;
-  }
+    const { data } = await axios.get('http://localhost:3000/api/restaurant');
+    return data;
+  };
 
-const { data } = useQuery('restaurants', getRestaurants);
+  const { data } = useQuery('restaurants', getRestaurants);
 
-return (
-  <CardWrapper>
-    {data?.map((item)=>{
-      console.log({item})
-      return(
-      <Card
-      title={item.name}
-      extra={<a href="#">More</a>}
-      style={{ width: 300 }}
-      bordered
-      size="small"
-      cover={<img alt="example" src={require(`./../../assets/images/restaurants/${item.id}.jpg`)}/>}
-      >
-    </Card>
-    )}
-    )}
-  </CardWrapper>
-  )
-}
+  return (
+    <CardWrapper>
+      {data?.map((item) => {
+        console.log({ item });
+        return (
+          <Card
+            title={item.name}
+            extra={<a href="#">More</a>}
+            style={{ width: 300 }}
+            bordered
+            size="small"
+            cover={
+              <img
+                alt="example"
+                src={require(`./../../assets/images/restaurants/${item.id}.jpg`)}
+              />
+            }
+          ></Card>
+        );
+      })}
+    </CardWrapper>
+  );
+};
 
-export default Restaurants
+export default Restaurants;
