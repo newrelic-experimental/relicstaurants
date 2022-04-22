5908cbb9def065c1c379f3b10e89f83e9049114b
kisielewskik <k.kisielewski.k@gmail.com>
2022-04-22 11:31:29 +0200

    feat: restaurants fetch and display

---
+++ src/components/common/restaurants/card-wrapper/card-wrapper.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const CardWrapper = styled.div`
+  width: 1400px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+`;
--- src/views/restaurants/restaurants.js
+++ src/views/restaurants/restaurants.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import { useQuery } from 'react-query';
 import { useLocation } from 'react-router';
+import { Card } from 'antd';
+import { CardWrapper } from 'components/common';
 
 const Restaurants = () => {
   const location = useLocation();
-console.log(location)
-  return (
-    <div>Restaurants</div>
+
+  const getRestaurants = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/restaurant');
+  return data;
+  }
+
+const { data } = useQuery('restaurants', getRestaurants);
+
+return (
+  <CardWrapper>
+    {data?.map((item)=>{
+      console.log({item})
+      return(
+      <Card
+      title={item.name}
+      extra={<a href="#">More</a>}
+      style={{ width: 300 }}
+      bordered
+      size="small"
+      cover={<img alt="example" src={require(`./../../assets/images/restaurants/${item.id}.jpg`)}/>}
+      >
+    </Card>
+    )}
+    )}
+  </CardWrapper>
   )
 }
 
