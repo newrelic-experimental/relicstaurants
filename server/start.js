e12d3fbf0c698358e169d51a461010a299d60540
kisielewskik <k.kisielewski.k@gmail.com>
2022-05-13 12:09:10 +0200

    feat: implement instrumentation

--- server/start.js
+++ server/start.js
@@ -1,5 +1,5 @@
 var PORT = process.env.PORT || 3000;
 var STATIC_DIR = __dirname + '/../build';
 var DATA_FILE = __dirname + '/data/restaurants.json';
-
+require('newrelic');
 require('./index').start(PORT, STATIC_DIR, DATA_FILE);
