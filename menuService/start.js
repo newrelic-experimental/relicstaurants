5b0d738b0947a492ac1360a275f4355bd06efef0
JakubKotkowiak <cckotkowiak@gmail.com>
2022-09-08 15:29:08 +0200

    refactor: add agent

--- menuService/start.js
+++ menuService/start.js
@@ -2,4 +2,6 @@ var PORT = process.env.PORT || 3003;
 var STATIC_DIR = __dirname + '/../build';
 var DATA_FILE = __dirname + '/../data/restaurants.json';
 
+require('newrelic');
+
 require('./index').start(PORT, STATIC_DIR, DATA_FILE);
